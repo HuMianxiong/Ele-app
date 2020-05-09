d193c3552d45fb51bf9a0ddfd0252ad0a3b44ab3
hmx <757134184@qq.com>
2020-05-09 23:05:04 +0800

    city

--- ele-app/src/router/index.js
+++ ele-app/src/router/index.js
@@ -1,16 +1,51 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Index from '../views/index.vue'
+
 
 Vue.use(VueRouter)
 
   const routes = [
   {
     path: '/',
-    name: 'index',
-    component: Index
+    // name: 'index',
+    component: ()=>import("../views/index.vue"),
+    children:[
+      {
+        path:'',
+        redirect:'/home'
+      },
+      {
+        path:"/home",
+        name:"home",
+        component:()=>import("../views/Home.vue")
+      },
+      {
+        path:"/order",
+        name:"order",
+        component:()=>import("../views/Order.vue")
+      },
+      {
+        path:'/me',
+        name:"me",
+        component:()=>import("../views/Me.vue")
+      }
+    ]
   },
- 
+  {
+    path:'/login',
+    name:'login',
+    component:()=>import("../views/Login.vue")
+  },
+  {
+    path:'/address',
+    name:'address',
+    component:()=>import('../views/Address.vue')
+  },
+  {
+    path:'/city',
+    name:'city',
+    component:()=>import('../views/City.vue')
+  }
 ]
 
 const router = new VueRouter({
@@ -19,4 +54,16 @@ const router = new VueRouter({
   routes
 })
 
+//路由守卫
+router.beforeEach((to,from,next)=>{
+  // const isLogin = localStorage.ele_login?true:false;
+  if(to.path === '/login'){
+    next();
+  }
+  else{
+    //是否在登录状态下
+    // isLogin?next():next("/login")
+    next()
+  }
+})
 export default router
